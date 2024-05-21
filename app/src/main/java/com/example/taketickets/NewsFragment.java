789f534fc2627b145909eec49e7f508aa4444eb9
Semyon <semyonkarabanov@gmail.com>
2024-05-21 21:32:41 +0300

    колхозный Adapter для RecyclerView +

--- app/src/main/java/com/example/taketickets/NewsFragment.java
+++ app/src/main/java/com/example/taketickets/NewsFragment.java
@@ -20,55 +20,12 @@ import com.example.taketickets.adapters.SimpleAdapter;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NewsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NewsFragment extends Fragment {
     public Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public NewsFragment() { }
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NewsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,20 +44,25 @@ public class NewsFragment extends Fragment {
         TextView textView = view.findViewById(R.id.textView_RecyclerView_News);
         Button button = view.findViewById(R.id.imageView_RecyclerNews);
 
+        // Инициализируем RecyclerView
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView_News);
+        // Устанавливаем для RecyclerView разметку
+        recyclerView.setLayoutManager(new LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL,false));
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        // Создаём 2 списка с String и ImageView(id)
+        List<String> items = Arrays.asList("Майор Гром: Игра", "Министерство джентельменских дел");
+        List<Integer> items2 = Arrays.asList(R.drawable.mayor_grom,R.drawable.ministerstvo);
 
-        List<String> items = Arrays.asList("Майор Гром: Игра", "Джентельмены");
-
-        List<Integer> items2 = Arrays.asList(R.drawable.logo,R.drawable.icon_tickets);
-
+        // Инициализация нашего адаптера(картинка и текст)
         SimpleAdapter adapter = new SimpleAdapter(items,items2);
+        // Устанавливаем адаптер
         recyclerView.setAdapter(adapter);
-
 
     }
 
+
+    //? метод, потому что раньше RecyclerView не отображался( не передавался context почему)
+    // TODO: Понять метод
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
